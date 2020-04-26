0ece0c1c9955c662a5c083a6d6f26d57716ec1c2
flipflop133 <fbechet45@gmail.com>
2020-04-26 19:42:32 +0200

    update dotfiles

--- backup.sh
+++ backup.sh
@@ -29,7 +29,6 @@ directories=(
 # backup files
 backup(){
     # copy dot files to backup directory
-    echo "Copying files..."
     echo -e "\e[96mCopying files...\e[0m"
     for dir in ${directories[@]}; do
         cp -R -f $dir $dot
