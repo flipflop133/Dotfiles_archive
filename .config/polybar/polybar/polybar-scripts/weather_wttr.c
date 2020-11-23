08fc3be42c41f571bfc6454b154900a39a7cc53c
flipflop133 <fbechet45@gmail.com>
2020-11-23 20:27:12 +0100

    clean dotfiles

--- .config/polybar/polybar/polybar-scripts/weather_wttr.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-int main(int argc, char *argv[]) {
-  char command[50] = "curl -s \"wttr.in/Saint-Léger,Belgium?format=1\"";
-  execl("/bin/bash", "bash", "-c", command, NULL);
-  return 0;
-}
